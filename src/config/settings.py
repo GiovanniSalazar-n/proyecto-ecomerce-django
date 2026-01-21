7307da19f1f74c93229c335b18fd55fe395a5b73
GiovanniSalazar-n <gial210802@gmail.com>
2026-01-21 00:42:04 -0600

    Django Models & Admin

--- src/config/settings.py
+++ src/config/settings.py
@@ -37,6 +37,7 @@ LOGIN_URL="/admin/login/"
 INSTALLED_APPS = [
     "pages.apps.PagesConfig",
     "ecomerce.apps.EcomerceConfig",
+    "base.apps.BaseConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
